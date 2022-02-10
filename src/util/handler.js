a556405676fd8445b24a766d0c10afa23f881ae6
TheBetterHighground <69619089+TheBetterHighground@users.noreply.github.com>
2022-02-10 16:52:19 -0500

    Enabling CORS

--- src/util/handler.js
+++ src/util/handler.js
@@ -13,7 +13,13 @@ export default function handler(lambda) {
 
         return {
             statusCode,
-            body: JSON.stringify(body)
+            body: JSON.stringify(body),
+            // here we are adding in the default CORS headers to allow for HTTP requests to the back-end server API
+            // Note that this does not enable CORS for the S3 bucket as that is handled separately.
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+            },
         };
     };
 }
