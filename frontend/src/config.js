e08a65dbc315ae3110ad8b4600e8eb4628df3367
TheBetterHighground <69619089+TheBetterHighground@users.noreply.github.com>
2022-02-14 10:40:38 -0500

    Setting up the React app

---
+++ frontend/src/config.js
@@ -0,0 +1,21 @@
+const config = {
+	s3: {
+		REGION: process.env.REACT_APP_REGION,
+		BUCKET: process.env.REACT_APP_BUCKET,
+	},
+	apiGateway: {
+		REGION: process.env.REACT_APP_REGION,
+		URL: process.env.REACT_APP_API_URL,
+	},
+	cognito: {
+		REGION: process.env.REACT_APP_REGION,
+		USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
+		APP_CLIENT_ID: process.env.REACT_APP_USER_POOL_CLIENT_ID,
+		IDENTITY_POOL_ID: process.env.REACT_APP_IDENTITY_POOL_ID,
+	},
+};
+
+export default config;
+// the above code will load the environment that's set up from the serverless backend
+// It will export a configuration JSON that provides the necessary data for any processes that
+// will be executed via the frontend.
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,39 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Amplify } from "aws-amplify";
+import config from "./config";
+
+/*
+Amplify will reference Cognito as Auth, S3 as Storage and API Gateway as API.
+the mandatory sign in is set to true because users must be logged in before they can interact with the application.
+With amplify, we can work with multiple API's but for now we're just setting it to notes via the name: "notes" down in endpoints.
+Amplify.configure() is a function created by AWS that allows us to specifically say
+which resources we want to interact with via the backend.
+*/
+Amplify.configure({
+	Auth: {
+		mandatorySignIn: true,
+		region: config.cognito.REGION,
+		userPoolId: config.cognito.USER_POOL_ID,
+		identityPoolId: config.cognito.IDENTITY_POOL_ID,
+		userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+	},
+	Storage: {
+		region: config.s3.REGION,
+		bucket: config.s3.BUCKET,
+		identityPoolId: config.cognito.IDENTITY_POOL_ID,
+	},
+	API: {
+		endpoints: [
+			{
+				name: "notes",
+				endpoint: config.apiGateway.URL,
+				region: config.apiGateway.REGION,
+			},
+		],
+	},
+});
 
 ReactDOM.render(
 	<React.StrictMode>
